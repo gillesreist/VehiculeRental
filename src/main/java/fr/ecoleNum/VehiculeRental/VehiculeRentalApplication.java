21fa7b6899631f0996c06a967858152f60887877
gilles.reist <gilles.reist@le-campus-numerique.fr>
2023-09-07 13:18:21 +0200

    project starter

---
+++ src/main/java/fr/ecoleNum/VehiculeRental/VehiculeRentalApplication.java
@@ -0,0 +1,13 @@
+package fr.ecoleNum.VehiculeRental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiculeRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiculeRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/ecoleNum/VehiculeRental/VehiculeRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ecoleNum.VehiculeRental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiculeRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
